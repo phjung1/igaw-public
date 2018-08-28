a911db3870982e4cdf75df1a29060497b1be0c87
phjung1 <36128943+phjung1@users.noreply.github.com>
2018-08-29 07:13:24 +0900

    초기 커밋.

---
+++ igaw-public/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace igaw_public
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var file = new FileInfo(@"wwwroot\index.html");
+                byte[] buffer;
+                if (file.Exists)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.ContentType = "text/html";
+
+                    buffer = File.ReadAllBytes(file.FullName);
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    context.Response.ContentType = "text/plain";
+                    buffer = Encoding.UTF8
+                        .GetBytes("Unable to find the requested file");
+                }
+
+                context.Response.ContentLength = buffer.Length;
+
+                using (var stream = context.Response.Body)
+                {
+                    await stream.WriteAsync(buffer, 0, buffer.Length);
+                    await stream.FlushAsync();
+                }
+            });
+        }
+    }
+}
